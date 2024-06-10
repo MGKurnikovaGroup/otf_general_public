0372500fbd2a9d6ab4cb0ee39a19507b07cd6871
Forest Ma <forestm@andrew.cmu.edu>
2024-06-10 12:54:54 -0400

    main function, frac arg

--- lysozyme_test_case_restraints/auto_restraint.sh
+++ lysozyme_test_case_restraints/auto_restraint.sh
@@ -6,8 +6,8 @@ do
 
         cd $X
 	cp ../auto_restraint_files/* .
-	./cpptraj.restraint.sh
-        python3 restraint_analysis.py
+        python3 restraint_analysis.py 0.5
+        #argument is the chosen cutoff fraction
         cd ..
 done
 
