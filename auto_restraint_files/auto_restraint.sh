ee50445083abb3c2b0f0b9dea532ffb4adc746a5
Forest Ma <forestm@andrew.cmu.edu>
2024-06-13 15:19:23 -0400

    style

--- auto_restraint_files/auto_restraint.sh
+++ auto_restraint_files/auto_restraint.sh
@@ -6,6 +6,7 @@ do
 
         cd $X
 	cp ../auto_restraint_files/* .
+        ./cpptraj.restraint.sh
         python3 restraint_analysis.py 0.5 md-complex/BB.avg.dat md-complex/BB2.avg.dat setup/lig_tleap.mol2 complex-repres.pdb
         #float argument is the chosen cutoff fraction
         cd ..
