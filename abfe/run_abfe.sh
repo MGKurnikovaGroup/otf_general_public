1bb3bf043477305814ef9e4aaec9fea7cb27fa22
Forest Ma <forestm@andrew.cmu.edu>
2024-07-02 15:08:03 -0400

    abfe shell

--- abfe/run_abfe.sh
+++ abfe/run_abfe.sh
@@ -6,9 +6,12 @@ for X in "$@"
 do
 	cd $X
 	cp $mypcl/*.py .
-	python3 dcrg_main.py $mypcl --initial_time 0.5 --additional_time 0.25
-	python3 rtr_main.py $mypcl --initial_time 0.5 --additional_time 0.25
-	python3 water_main.py $mypcl --initial_time 0.5 --additional_time 0.25
+	# python3 dcrg_main.py $mypcl --initial_time 0.5 --additional_time 0.25
+	# python3 rtr_main.py $mypcl --initial_time 0.5 --additional_time 0.25
+	# python3 water_main.py $mypcl --initial_time 0.5 --additional_time 0.25
+	python3 abfe_main.py $mypcl dcrg --initial_time 0.5 --additional_time 0.25
+	python3 abfe_main.py $mypcl rtr --initial_time 0.5 --additional_time 0.25
+	python3 abfe_main.py $mypcl water --initial_time 0.5 --additional_time 0.25
 	cd ..
 	mv $X ~/trajectories2/ALK2/fifth
 done
