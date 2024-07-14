f8fe37700174deceda8eb35d3badf47082c64ce1
Forest Ma <forestm@andrew.cmu.edu>
2024-07-13 23:37:26 -0400

    a

--- otf_abfe/run_abfe.sh
+++ otf_abfe/run_abfe.sh
@@ -127,4 +127,4 @@ do
 	python3 abfe_main.py "../$mypcl" "$type" --convergence_cutoff "$convergence_cutoff" --initial_time "$initial_time" --additional_time "$additional_time" --first_max "$first_max" --second_max "$second_max" --schedule "$schedule" --num_windows "$num_windows" --custom_windows "$custom_windows"
 	cd ..
 	mv $X "$move_to"
-done
+done 
