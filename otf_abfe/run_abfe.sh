9bb54d8447e6f1681fa7ebe825b0ce0a2179f310
Ben Koby grad rotation <bkoby@h44.chem.cmu.edu>
2025-03-05 15:07:07 -0500

    fixed run_abfe

--- otf_abfe/run_abfe.sh
+++ otf_abfe/run_abfe.sh
@@ -140,6 +140,7 @@ do
 	echo =====  $X  =======================
 	cd $X
 	cp $mypcl/*.py .
+	cp $mypcl/../convergence_test.py .
 	python3 abfe_main.py "$mypcl" "$type" --convergence_cutoff "$convergence_cutoff" --initial_time "$initial_time" --additional_time "$additional_time" --first_max "$first_max" --second_max "$second_max" --schedule "$schedule" --num_windows "$num_windows" --custom_windows "$custom_windows" --rtr_window "$rtr_window" --sssc "$sssc" --equil_rest "$equil_restr" --fpn "$frames_per_ns"
 	cd ..
 	mv $X "$move_to"
