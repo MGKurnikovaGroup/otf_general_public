df1b62c0cd9d9005789b7b5056af56d2d64ac78a
Group account for 2025 SURA students <sura2025@h40.chem.cmu.edu>
2025-06-05 16:15:29 -0400

    Pythonifying rbfe_all_prep

---
+++ otf_rbfe/all_prep_rbfe.py
@@ -0,0 +1,29 @@
+##/bin/bash
+import glob
+import shutil
+import os
+import sys
+import subprocess
+
+# Below we go to the user's home directory and assign the path to "otf_rbfe" to the variable mypcl
+home_dir = os.pathexpanduser("~")
+mypcl = os.path.join(home_dir, "otf_rbfe") #mypcl=$(realpath $(find ~/ -type d -name "otf_rbfe"))
+
+
+
+for X in sys.argv[1:]:
+	for f in glob.glob(os.path.join(mypcl, "*.py")):  #cp $mypcl/*.py $X
+		shutil.copy(f, X)
+
+	for f in glob.glob(os.path.join(mypcl, "site", "*.sh")): #cp $mypcl/site/*.sh $X/site
+		shutil.copy(f, os.path.join(X, "site"))
+
+	for f in glob.glob(os.path.join(mypcl, "site" "cpp*")): #cp $mypcl/water/*.sh $X/water
+		shutil.copy(f, os.path.join(X, "water"))
+
+	for f in glob.glob(os.path.join(mypcl, "site", "*.sh")): #cp $mypcl/water/cpp* $X/water
+		shutil.copy(f, os.path.join(X, "water"))
+
+	os.chdir(X)
+	subprocess.run(['python3', "otf_rbfe/write_scmask.py"], check = True)
+	os.chdir("..")	
