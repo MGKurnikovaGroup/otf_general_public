24262511eb52d6e587615a4de76604b349edb21d
Group account for 2025 SURA students <sura2025@h40.chem.cmu.edu>
2025-06-08 21:09:40 -0400

    Completed translation

--- otf_abfe/abfe_all_prep.py
+++ otf_abfe/abfe_all_prep.py
@@ -28,7 +28,7 @@ Help messages are specified in the description and help variables
 def parse_args():
     parser = argparse.ArgumentParser(
             description = "", # Comment displayed at the top when --help is called
-            formatter_class = RawDescriptionHelpFormatter
+            formatter_class = argparse.RawDescriptionHelpFormatter
     )
 
     parser.add_argument(
@@ -106,9 +106,54 @@ def main():
             shutil.copy(f, target / 'rtr') # cp $mypcl/rtr/*py rtr/
 
         # Use names of selected ligand atoms to generate cpp.get-vb.in
-        myla1, myla2, myla3 = read_atoms(target / 'vbla.txt')
-        mypa1, mypa2, mypa3 = read_atoms(target / 'vbla2.txt')
-        print("\nvbla.txt:")
-        print((target / 'vbla.txt').read_text())
+        myla1, myla2, myla3 = read_atoms(target / 'vbla.txt') # myla1=$(awk '{print $1}' vbla.txt) ...
+        mypa1, mypa2, mypa3 = read_atoms(target / 'vbla2.txt') # mypa1=$(awk '{print $1}' vbla2.txt) ...
+        print("\nvbla.txt:") # echo; echo "vbla.txt:"
+        print((target / 'vbla.txt').read_text()) # cat vbla.txt;
 
+        # Edit template.cpp.get-vb.in to user inputted angle, distance, dihedral values
+        templ_content = f"""parm complex.prmtop
+reference complex.inpcrd
+
+rst :MOL@LATOM1 :PROTATOM1 reference width 10.0 rk2 {args.distance} rk3 {args.distance} out k.RST
+rst :MOL@LATOM1 :PROTATOM1 :PROTATOM2 reference width 90.0 rk2 {args.angle} rk3 {args.angle} out k.RST
+rst :MOL@LATOM1 :PROTATOM1 :PROTATOM2 :PROTATOM3 reference width 90.0 rk2 {args.dihedral} rk3 {args.dihedral} out k.RST
+rst :MOL@LATOM2 :MOL@LATOM1 :PROTATOM1 reference width 90.0 rk2 {args.angle} rk3 {args.angle} out k.RST
+rst :MOL@LATOM2 :MOL@LATOM1 :PROTATOM1 :PROTATOM2 reference width 90.0 rk2 {args.dihedral} rk3 {args.dihedral} out k.RST
+rst :MOL@LATOM3 :MOL@LATOM2 :MOL@LATOM1 :PROTATOM1 reference width 90.0 rk2 {args.dihedral} rk3 {args.dihedral} out k.RST
+
+run
+"""
+        (target / 'template.cpp.get-vb.in').write_text(templ_content) # cat <<EOF > template.cpp.get-vb.in ... EOF
         
+        # Write to cpp.get-vb.in
+        cpp_content = templ_content.replace('LATOM1', myla1) # sed "s/LATOM1/$myla1/" template.cpp.get-vb.in > cpp.get-vb.in
+        cpp_content = cpp_content.replace('LATOM2', myla2) # sed -i "s/LATOM2/$myla2/" cpp.get-vb.in
+        cpp_content = cpp_content.replace('LATOM3', myla3) # sed -i "s/LATOM3/$myla3/" cpp.get-vb.in
+        cpp_content = cpp_content.replace('PROTATOM1', mypa1) # sed -i "s/PROTATOM1/$mypa1/" cpp.get-vb.in
+        cpp_content = cpp_content.replace('PROTATOM2', mypa2) # sed -i "s/PROTATOM2/$mypa2/" cpp.get-vb.in
+        cpp_content = cpp_content.replace('PROTATOM3', mypa3) # sed -i "s/PROTATOM3/$mypa3/" cpp.get-vb.in
+        (target / 'cpp.get-vb.in').write_text(cpp_content) # Write to cpp.get-vb.in
+
+        print("\ncpp.get-vb.txt:") # echo; echo "cpp.get-vb.txt:"
+        print(cpp_content) # cat cpp.get-vb.in;
+
+        # Create protein-ligand restraints
+        with open('std.get-vb.txt', 'w') as stdout_file:
+            subprocess.run(['cpptraj', '-i', 'cpp.get-vb.in'], stdout=stdout_file) # cpptraj -i cpp.get-vb.in > std.get-vb.txt
+        print("PROTEIN-LIGAND RESTRAINTS:") # cpptraj -i cpp.get-vb.in > std.get-vb.txt
+        subprocess.run(['grep', 'rk2', 'k.RST']) # grep r2 k.RST
+        shutil.copy('k.RST', target / 'dcrg+vdw') # cp k.RST dcrg+vdw/
+        shutil.copy('k.RST', target / 'rtr') # cp k.RST rtr/
+
+        # Copy water files
+        shutil.copy(target / 'setup' / 'ligwat.inpcrd', target / 'water-dcrg+vdw' / 'ligwat.inpcrd') # cp setup/ligwat.inpcrd ./water-dcrg+vdw/
+        # Copy all executable files
+        for f in (mypcl / 'water-dcrg+vdw').glob('*.sh'):
+            shutil.copy(f, target / 'water-dcrg+vdw') # cp $mypcl/water-dcrg+vdw/*sh water-dcrg+vdw/
+        shutil.copy(target / 'setup' / 'ligwat.prmtop', target / 'water-dcrg+vdw' / 'ligwat.prmtop') # cp setup/ligwat.prmtop ./water-dcrg+vdw/
+
+        os.chdir(mywd) # cd $mywd
+
+if __name__ == '__main__':
+    main()
