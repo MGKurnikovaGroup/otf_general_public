7c3bbe5fb3b6ea86a25a3faa4f505b77fcc560dd
Group account for 2025 SURA students <sura2025@h40.chem.cmu.edu>
2025-06-13 13:50:31 -0400

    Repaired raw_name in cmd, added explanation to middle_name

--- otf_helper.py
+++ otf_helper.py
@@ -14,7 +14,7 @@ class parameters():
         self.curr_name = curr_name # current file name (file name of the destination of the first copy command, exclude filename extension)
         self.prev_dir = prev_dir # previous directory (if previous directory is ../complex.inpcrd, then prev_dir is "..", prev_name is "complex" or "")
         self.prev_name = prev_name # previous file name
-        self.middle_name = middle_name
+        self.middle_name = middle_name # file name between copies
         self.special_name = special_name # name before .in that is different from all others
         self.raw_name = raw_name # name just before .prmtop
 
@@ -29,7 +29,7 @@ class parameters():
             cmd = [
                 'pmemd.cuda', '-O',
                 '-i', f"{self.curr_dir}/{self.special_name}.in",
-                '-p', self.cwd.parent / f"{self.middle_name}.prmtop",
+                '-p', self.cwd.parent / f"{self.raw_name}.prmtop",
                 '-c', self.cwd / self.curr_dir / f"{self.middle_name}.inpcrd",
                 '-x', self.cwd / self.curr_dir / f"{self.curr_name}_00.nc",
                 '-o', self.cwd / self.curr_dir / f"{self.curr_name}_00.out",
@@ -39,7 +39,7 @@ class parameters():
             cmd = [
                 'pmemd.cuda', '-O',
                 '-i', f"{self.curr_dir}/{self.special_name}.in",
-                '-p', self.cwd.parent / f"{self.middle_name}.prmtop",
+                '-p', self.cwd.parent / f"{self.raw_name}.prmtop",
                 '-c', self.cwd / self.curr_dir / f"{self.middle_name}.inpcrd",
                 '-x', self.cwd / self.curr_dir / f"{self.curr_name}.nc",
                 '-o', self.cwd / self.curr_dir / f"{self.curr_name}.out",
