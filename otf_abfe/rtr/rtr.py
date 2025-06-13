cbbfdf105814fc266a624b0053e05b2a8dccd123
Group account for 2025 SURA students <sura2025@h40.chem.cmu.edu>
2025-06-13 16:07:01 -0400

    Added parameter

--- otf_abfe/rtr/rtr.py
+++ otf_abfe/rtr/rtr.py
@@ -5,19 +5,14 @@ import argparse
 import shutil
 from pathlib import Path
 
-def run_rtr():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "dirs", nargs = "+", type = Path
-    )
-    args = parser.parse_args()
-
+def run_rtr(lam):
     print("Reading in input files from the directory")
 
-    os.chdir(args.dirs[0])
+    target = f"la-{lam}"
+    os.chdir(target)
     
     p_prod = parameters(
-        args.dirs[0], "production", "..", "complex_prod", "prod",
+        Path(target), "production", "..", "complex_prod", "prod",
         "complex_prod", "complex_prod", "prod", "complex"
     )
     p_prod.run_pmemd(first = True, last = True)
