7c79e3075d4b4cafa4f29a1cc43393305af752da
Group account for 2025 SURA students <sura2025@h40.chem.cmu.edu>
2025-06-13 14:37:22 -0400

    Completed translation

---
+++ otf_abfe/rtr/md-lambda.py
@@ -0,0 +1,27 @@
+import otf_helper
+import os
+import argparse
+import shutil
+from pathlib import Path
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "dirs", nargs = "+", type = Path
+    )
+    args = parser.parse_args()
+
+    print("Reading in input files from the directory")
+
+    os.chdir(args.dirs[0])
+    
+    p_prod = parameters(
+        args.dirs[0], "production", "..", "complex_prod", "prod",
+        "complex_prod", "complex_prod", "prod", "complex"
+    )
+    p_prod.run_pmemd(first = True, last = True)
+
+    os.chdir("..")
+
+if __name__ == '__main__':
+    main()
