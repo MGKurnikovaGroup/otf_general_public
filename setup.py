105e5ef8b9b765cc1a050b6e26c457655d37a0a5
Isk-101 <iskanergashev@gmail.com>
2025-08-03 13:40:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 setup(
-    name='otf_general',  # Name of your overall repo/package
+    name='otf_general_public',  # Name of your overall repo/package
     version='0.1.0',
     author='Iskandar E, Sihao J',
     description='On-the-Fly RBFE/ABFE simulation framework for AMBER20',
-    packages=find_packages(include=['otf_rbfe', 'otf_rbfe.*', 'otf_abfe', 'otf_abfe.*']),  # include submodules
-    install_requires=[
-        'pymbar==4.0.3',
-        'alchemlyb==2.3.1',
-        'scipy==1.8.1'
-    ],
+    packages=find_packages(),  # include submodules
+    # install_requires=[
+    #     'pymbar==4.0.3',
+    #     'alchemlyb==2.3.1',
+    #     'scipy==1.8.1'
+    # ],
     python_requires='>=3.0',
     include_package_data=True,
 )
