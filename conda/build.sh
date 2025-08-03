63b31210e9aa1a947db368be28c59b97dcfc94dd
Isk-101 <iskanergashev@gmail.com>
2025-08-03 13:33:42 -0400

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -2,5 +2,5 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 # install the package using setup.py from the repo root
-$PYTHON setup.py install
+$PYTHON -m pip install . --no-deps --ignore-installed -vv
 
